add9d7c6308995984de55e2ac29d80346208b0cd
DarrellGehring <darrell@wasatch-solutions.com>
2020-06-08 17:59:04 -0600

    Asdas

--- HW3.c
+++ HW3.c
@@ -2,10 +2,10 @@
 int main(int numForks, char *fileName[]) {
 
 	if (numForks = NULL || (numForks != 1 && numForks != 4)) {
-		printf("Invalid Parameter: only takes 1 or 4");
+		printf("Invalid Parameter: only takes 1 or 4\n");
 		return 1;
 	}
 
-	printf("Hello, World!");
+	printf("Hello, World!\n");
 	return 0;
 }
