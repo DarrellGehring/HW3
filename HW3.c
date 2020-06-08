20fc297bd80f00b0a44f6bea2d873c0232ab49db
DarrellGehring <darrell@wasatch-solutions.com>
2020-06-08 17:54:09 -0600

    A

--- HW3.c
+++ HW3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(int numForks, char *fileName[]) {
 
-	if (numForks != 1 && numForks != 4) {
+	if (!numForks || numForks != 1 && numForks != 4) {
 		printf("Invalid Parameter: only takes 1 or 4");
 		return 1;
 	}
