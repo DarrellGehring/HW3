c65010207bdf3f0618c3f6ff7990c15d3e2e654a
DarrellGehring <darrell@wasatch-solutions.com>
2020-06-10 17:10:34 -0600

    assa

--- HW3.c
+++ HW3.c
@@ -39,6 +39,7 @@ int main(int numArgs, char *args[]) {
 			close(0); //close stdin
 			close(cp[0]); //close pipe in
 			execl("./minMax", "./minMax", "1", *args[2], (char *)0);  //note: All the arguments in exec have to be strings.
+			die(execl);
 		}
 		else
 		{
