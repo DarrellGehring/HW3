a52dcf06593939083eed62fbd209a2245d3a1bc1
DarrellGehring <darrell@wasatch-solutions.com>
2020-06-10 16:29:40 -0600

    assa

--- HW3.c
+++ HW3.c
@@ -16,8 +16,19 @@ int main(int numArgs, char *args[]) {
 		return -1;
 	}
 
+	int cp[2];
+
+	if (pipe(cp) < 0) {
+		printf("didn't work, couldn't not establish pipe.\n");
+		return -1;
+	}
+
 	if (*args[1] == '1') {
 		printf("Exec:: \n");
+		close(1); //close stdout
+		dup2(cp[1], 1); //move stdout to pipe of cp[1]
+		close(0); //close stdin
+		close(cp[0]); //close pipe in
 		execl("minMax", "minMax", "1", *args[2], (char *)0);
 		printf("Exec|| \n");
 	}
