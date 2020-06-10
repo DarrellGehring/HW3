5f95ad7383040e52dd177774dced9d6fda336c2c
DarrellGehring <darrell@wasatch-solutions.com>
2020-06-09 21:51:49 -0600

    assa

--- HW3.c
+++ HW3.c
@@ -1,5 +1,9 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main(int numArgs, char *args[]) {
 	if (numArgs != 3) {
@@ -12,7 +16,28 @@ int main(int numArgs, char *args[]) {
 		return -1;
 	}
 
-	printf("%s", args[2]);
+	const char * filename = *args[2];
 
+	FILE * readF = fopen(filename, "rb");
+
+	if (readF) {
+		int pid = getpid();
+		fseek(ft, 0, SEEK_END); //go to end of file
+		long size = ftell(ft); //what byte in file am I at?
+		fseek(ft, 0, SEEK_SET); //go to beginning of file
+		int num = (int)size / (int)sizeof(int);
+		printf("size of the file: %li ,sizeof(int) = %i\n, the number of numbers = %i\n\n", size, (int) sizeof(int), num);
+		int i;
+		for (i = 0; i < num; i++) {
+			int temp = 0;
+			fread(&temp, sizeof(int), 1, ft);
+			printf("%i: %i\t", pid, temp);
+		}
+		fclose(ft);
+	}
+	else {
+		printf("Error: no file found with that name.\n");
+	}
+	printf("\n");
 	return 0;
 }
