ac066bbd9cdc8b10e73fcdf8fc7333fcc39bced8
DarrellGehring <darrell@wasatch-solutions.com>
2020-06-09 21:54:53 -0600

    A

--- HW3.c
+++ HW3.c
@@ -16,28 +16,6 @@ int main(int numArgs, char *args[]) {
 		return -1;
 	}
 
-	const char * filename = *args[2];
-
-	FILE * readF = fopen(filename, "rb");
-
-	if (readF) {
-		int pid = getpid();
-		fseek(ft, 0, SEEK_END); //go to end of file
-		long size = ftell(ft); //what byte in file am I at?
-		fseek(ft, 0, SEEK_SET); //go to beginning of file
-		int num = (int)size / (int)sizeof(int);
-		printf("size of the file: %li ,sizeof(int) = %i\n, the number of numbers = %i\n\n", size, (int) sizeof(int), num);
-		int i;
-		for (i = 0; i < num; i++) {
-			int temp = 0;
-			fread(&temp, sizeof(int), 1, ft);
-			printf("%i: %i\t", pid, temp);
-		}
-		fclose(ft);
-	}
-	else {
-		printf("Error: no file found with that name.\n");
-	}
 	printf("\n");
 	return 0;
 }
