d1975a841891892dd37d7ab3c3aa9ec259cb1559
DarrellGehring <darrell@wasatch-solutions.com>
2020-06-10 19:21:28 -0600

    assa

--- HW3.c
+++ HW3.c
@@ -36,7 +36,7 @@ int main(int numArgs, char *args[]) {
 			return -1;
 		}
 
-		char* filePath = *args[2];
+		char* filePath = args[2];
 		
 
 		int pid = fork();
@@ -48,7 +48,7 @@ int main(int numArgs, char *args[]) {
 			dup2(cp[1], 1); //move stdout to pipe of cp[1]
 			close(0); //close stdin
 			close(cp[0]); //close pipe in
-			int retVal = execl("minMax", "minMax", "1", (char *)filePath);  //note: All the arguments in exec have to be strings.
+			int retVal = execl("minMax", "minMax", "1", (char *)filePath, NULL);  //note: All the arguments in exec have to be strings.
 
 			if (retVal == -1) {
 				printf("exec was unsuccessful");
