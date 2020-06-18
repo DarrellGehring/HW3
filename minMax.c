5bdf003dc5c00d4d53e3de9ac81f79bd91ec7fb8
DarrellGehring <darrell@wasatch-solutions.com>
2020-06-17 21:02:16 -0600

    assa

--- minMax.c
+++ minMax.c
@@ -357,7 +357,7 @@ int main(int numArgs, char *args[]) {
 		fpos = ((i*nums) / 4);
 
 		// write to child process
-		//printf("Parent(%d): Sending file position to child\n", pid);
+		printf("Parent(%d): Sending file position to child\n", pid);
 		write(fd[i][1], &fpos, sizeof(fpos));
 
 		// wait for child responce
