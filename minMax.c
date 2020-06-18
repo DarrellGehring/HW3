02d1b35ab4725fe92fd6128e3c5ee784835fef23
DarrellGehring <darrell@wasatch-solutions.com>
2020-06-17 21:14:45 -0600

    assa

--- minMax.c
+++ minMax.c
@@ -262,150 +262,186 @@ int main(int numArgs, char *args[]) {
 		return -1;
 	}
 
-	// printf("Here!\n");
-
 	const char *filename = args[2];
 	// printf("Opening file:\n");
 	FILE * file = fopen(filename, "r");
-	// printf("Finished.\n");
 
-	int fd[8][2]; //parent+child pipe
-	// printf("Here2!\n");
-	int i, j, len, fpos = 0, val, count = 0, total = 0, min = -1, max = -1, minMin, maxMax;
-	// printf("Here3!\n");
-	pid_t pid;
+	if (file) {
+		if (*args[1] == '4') {
+			// printf("Here!\n");
+			// printf("Finished.\n");
 
-	// printf("Here4!\n");
+			int fd[8][2]; //parent+child pipe
+			// printf("Here2!\n");
+			int i, j, len, fpos = 0, val, count = 0, total = 0, min = -1, max = -1, minMin, maxMax;
+			// printf("Here3!\n");
+			pid_t pid;
 
-	fseek(file, 0, SEEK_END); //go to end of file
-	long size = ftell(file); //what byte in file am I at?
-	fseek(file, 0, SEEK_SET); //go to beginning of file
+			// printf("Here4!\n");
 
-	int nums = (int)size / (int)sizeof(int);
-	// printf("Here5!\n");
+			fseek(file, 0, SEEK_END); //go to end of file
+			long size = ftell(file); //what byte in file am I at?
+			fseek(file, 0, SEEK_SET); //go to beginning of file
 
-	// create all pipes
-	// printf("Hopefully will make 4 pipes.\n");
+			int nums = (int)size / (int)sizeof(int);
+			// printf("Here5!\n");
+
+			// create all pipes
+			// printf("Hopefully will make 4 pipes.\n");
 
 
-	for (i = 0; i < 8; i++) {
-		pipe(fd[i]);
-		// printf("Opening pipes for %d!\n", i);
-	}
+			for (i = 0; i < 8; i++) {
+				pipe(fd[i]);
+				// printf("Opening pipes for %d!\n", i);
+			}
 
-	// printf("After Pipes!\n");
+			// printf("After Pipes!\n");
 
-	for (i = 0; i < 4; i++)
-	{
-		if ((pid = fork()) == 0) // child process
-		{
-			pid = getpid();
-
-			// read from parent
-			len = read(fd[i][0], &fpos, sizeof(fpos));
-			if (len > 0)
+			for (i = 0; i < 4; i++)
 			{
-				fseek(file, fpos*(int)sizeof(int), SEEK_SET);
-				count = 0;
-				total = 0;
-
-				// printf("Child(%d): Recieved position: %d\n", pid, fpos);
-
-				// read from file starting at fpos
-				// add values read to a total value
-				while (count < (nums / 4))
+				if ((pid = fork()) == 0) // child process
 				{
-					// printf("Child(%d): Looping in %d.\n", pid, count);
-					int temp = 0;
-					fread(&temp, sizeof(int), 1, file);
-					
-					if (count == 0) {
-						min = temp;
-						max = temp;
+					pid = getpid();
+
+					// read from parent
+					len = read(fd[i][0], &fpos, sizeof(fpos));
+					if (len > 0)
+					{
+						fseek(file, fpos*(int)sizeof(int), SEEK_SET);
+						count = 0;
+						total = 0;
+
+						// printf("Child(%d): Recieved position: %d\n", pid, fpos);
+
+						// read from file starting at fpos
+						// add values read to a total value
+						while (count < (nums / 4))
+						{
+							// printf("Child(%d): Looping in %d.\n", pid, count);
+							int temp = 0;
+							fread(&temp, sizeof(int), 1, file);
+
+							if (count == 0) {
+								min = temp;
+								max = temp;
+							}
+							else {
+								if (max < temp) {
+									//printf("Child(%d): setting max to %d.\n", pid, max);
+									max = temp;
+								}
+
+								if (min > temp) {
+									//printf("Child(%d): setting min to %d.\n", pid, min);
+									min = temp;
+								}
+							}
+
+							count++;
+						}
+						//write to parent
+						write(fd[i + 4][1], &min, sizeof(min));
+						// printf("Child(%d): Sent %d as min to parent.\n", pid, min);
+						write(fd[i + 4][1], &max, sizeof(max));
+						// printf("Child(%d): Sent %d as max to parent.\n", pid, max);
+					}
+					else
+					{
+						// printf("Child(%d): Error with len\n", pid);
+					}
+
+					exit(0); //Not triggering?
+				}
+
+				// parent process
+				pid = getpid();
+
+				fpos = ((i*nums) / 4);
+
+				// write to child process
+				// printf("Parent(%d): Sending file position to child\n", pid);
+				write(fd[i][1], &fpos, sizeof(fpos));
+
+				// wait for child responce
+				len = read(fd[i + 4][0], &min, sizeof(min));
+
+				if (len > 0)
+				{
+					//printf("Parent(%d): Recieved %d as min from child.\n", pid, min);
+
+					if (i == 0) {
+						minMin = min;
 					}
 					else {
-						if (max < temp) {
-							//printf("Child(%d): setting max to %d.\n", pid, max);
-							max = temp;
-						}
-
-						if (min > temp) {
-							//printf("Child(%d): setting min to %d.\n", pid, min);
-							min = temp;
+						if (minMin > min) {
+							minMin = min;
 						}
 					}
-
-					count++;
+					//total += total;
+					//printf("Parent(%d): Total: %d\n", pid, total);
 				}
-				//write to parent
-				write(fd[i + 4][1], &min, sizeof(min));
-				// printf("Child(%d): Sent %d as min to parent.\n", pid, min);
-				write(fd[i + 4][1], &max, sizeof(max));
-				// printf("Child(%d): Sent %d as max to parent.\n", pid, max);
-			}
-			else
-			{
-				// printf("Child(%d): Error with len\n", pid);
-			}
+				else
+				{
+					printf("Parent(%d): Error with len\n", pid);
+				}
 
-			exit(0); //Not triggering?
-		}
+				len = read(fd[i + 4][0], &max, sizeof(max));
 
-		// parent process
-		pid = getpid();
+				if (len > 0)
+				{
+					//printf("Parent(%d): Recieved %d as max from child.\n", pid, max);
 
-		fpos = ((i*nums) / 4);
-
-		// write to child process
-		// printf("Parent(%d): Sending file position to child\n", pid);
-		write(fd[i][1], &fpos, sizeof(fpos));
-
-		// wait for child responce
-		len = read(fd[i + 4][0], &min, sizeof(min));
-
-		if (len > 0)
-		{
-			//printf("Parent(%d): Recieved %d as min from child.\n", pid, min);
-
-			if (i == 0) {
-				minMin = min;
-			}
-			else {
-				if (minMin > min) {
-					minMin = min;
+					if (i == 0) {
+						maxMax = max;
+					}
+					else {
+						if (maxMax < max) {
+							maxMax = max;
+						}
+					}
+					//total += total;
+					//printf("Parent(%d): Total: %d\n", pid, total);
+				}
+				else
+				{
+					printf("Parent(%d): Error with len\n", pid);
 				}
 			}
-			//total += total;
-			//printf("Parent(%d): Total: %d\n", pid, total);
 		}
-		else
-		{
-			printf("Parent(%d): Error with len\n", pid);
-		}
+		else {
+			int minMin, maxMax;
+			fseek(readF, 0, SEEK_END); //go to end of file
+			long size = ftell(readF); //what byte in file am I at?
+			fseek(readF, 0, SEEK_SET); //go to beginning of file
+			int num = (int)size / (int)sizeof(int);
+			//printf("size of the file: %li ,sizeof(int) = %i\n, the number of numbers = %i\n\n", size, (int) sizeof(int), num);
+			int i;
+			for (i = 0; i < num; i++) {
+				int temp = 0;
+				fread(&temp, sizeof(int), 1, readF);
+				//printf("%i: %i\t", pid, temp);
 
-		len = read(fd[i + 4][0], &max, sizeof(max));
+				if (i == 0) {
+					minMin = temp;
+					maxMin = temp;
+					//printf("%i: %i : %d\t\n", pid, temp, i);
+				}
 
-		if (len > 0)
-		{
-			//printf("Parent(%d): Recieved %d as max from child.\n", pid, max);
+				if (temp < min) {
+					//printf("Min was: %d Is now: %d\n", min, temp);
+					minMin = temp;
+				}
 
-			if (i == 0) {
-				maxMax = max;
-			}
-			else {
-				if (maxMax < max) {
-					maxMax = max;
+				if (temp > max) {
+					//printf("Max was: %d Is now: %d\n", max, temp);
+					maxMax = temp;
 				}
 			}
-			//total += total;
-			//printf("Parent(%d): Total: %d\n", pid, total);
 		}
-		else
-		{
-			printf("Parent(%d): Error with len\n", pid);
-		}
+	} else {
+		printf("Failed to read given file!");
 	}
-
+	fclose(readF);
 	printf("Minimum: %d\t Maximum: %d\n", minMin, maxMax);
 
 	return 0;
