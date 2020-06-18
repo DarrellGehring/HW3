650f8f8db58c220eef97c47d2304107a31102418
DarrellGehring <darrell@wasatch-solutions.com>
2020-06-17 20:00:40 -0600

    assa

--- minMax.c
+++ minMax.c
@@ -404,8 +404,7 @@ int main(int numArgs, char *args[]) {
 		{
 			printf("Parent(%d): Error with len\n", pid);
 		}
-		printf("Minimum: %d\t Maximum: %d\n", minMin, maxMax);
 	}
-
+	printf("Minimum: %d\t Maximum: %d\n", minMin, maxMax);
 	return 0;
 }
