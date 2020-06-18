93e5affcf730d8f612d59daaca5db69bfb1e24b6
DarrellGehring <darrell@wasatch-solutions.com>
2020-06-17 20:01:28 -0600

    assa

--- minMax.c
+++ minMax.c
@@ -357,7 +357,7 @@ int main(int numArgs, char *args[]) {
 		fpos = ((i*nums) / 4); // 5 is the offset of the file values
 
 		// write to child process
-		printf("Parent(%d): Sending file position to child\n", pid);
+		// printf("Parent(%d): Sending file position to child\n", pid);
 		write(fd[i][1], &fpos, sizeof(fpos));
 
 		// wait for child responce
@@ -365,7 +365,7 @@ int main(int numArgs, char *args[]) {
 
 		if (len > 0)
 		{
-			printf("Parent(%d): Recieved %d as min from child.\n", pid, min);
+			// printf("Parent(%d): Recieved %d as min from child.\n", pid, min);
 
 			if (i == 0) {
 				minMin = min;
@@ -387,7 +387,7 @@ int main(int numArgs, char *args[]) {
 
 		if (len > 0)
 		{
-			printf("Parent(%d): Recieved %d as max from child.\n", pid, max);
+			// printf("Parent(%d): Recieved %d as max from child.\n", pid, max);
 
 			if (i == 0) {
 				maxMax = max;
